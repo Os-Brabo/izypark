e8048f5a4281cde6140ccfb29e687f7f7a666ccd
LucasFlaquer <lucas.flaquer@gmail.com>
2022-04-07 21:11:37 -0300

    setup styled components and fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
+import { useFonts, Roboto_400Regular } from '@expo-google-fonts/roboto';
 import { StatusBar } from 'expo-status-bar';
+import AppLoading from 'expo-app-loading'
 import { StyleSheet, Text, View } from 'react-native';
+import { Background } from './src/components/Background';
+
+
+
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+  })
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <Background>
+    <View><Text>aaaa</Text></View>
+  </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/Background/index.tsx
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+
+export const Background = styled.view`
+  flex: 1;
+`
