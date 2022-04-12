b2ff9e7e33ff652fbab219a8771a6f6034e58b53
Lucas Flaquer <lucas.flaquer@gmail.com>
2022-04-12 20:38:47 -0300

    toast provider

--- App.tsx
+++ App.tsx
@@ -1,16 +1,21 @@
 import 'react-native-reanimated'
 import * as React from 'react';
-import { Button, View } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { Home } from './src/views/Home';
 import { useFonts, Roboto_400Regular, Roboto_700Bold, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import AppLoading from 'expo-app-loading'
 import { About } from './src/views/About';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Login } from './src/views/Authenticate/Login';
+import { ToasterProvider } from './src/hooks/Toaster';
+import { Toaster } from './src/components/Toaster';
+
 
 const Drawer = createDrawerNavigator();
-
+const Stack = createNativeStackNavigator();
 export default function App() {
+  const isAuthenticated = false;
   let [fontsLoaded] = useFonts({
     RobotoRegular: Roboto_400Regular,
     RobotoBlack: Roboto_700Bold,
@@ -19,14 +24,25 @@ export default function App() {
   if (!fontsLoaded) {
     return <AppLoading />;
   }
+  if(isAuthenticated)
+    return (
+      <ToasterProvider>
+        <NavigationContainer>
+          <Drawer.Navigator useLegacyImplementation initialRouteName="Home">
+            <Drawer.Screen name="Home" component={Home} options={{headerShown:false}} />
+            <Drawer.Screen name="Notifications" component={About} options={{headerShown:false}} />
+          </Drawer.Navigator>
+        </NavigationContainer>
+        <Toaster />
+      </ToasterProvider>
+    );
   return (
     <NavigationContainer>
-      <Drawer.Navigator useLegacyImplementation initialRouteName="Home">
-        <Drawer.Screen name="Home" component={Home} options={{headerShown:false}} />
-        <Drawer.Screen name="Notifications" component={About} options={{headerShown:false}} />
-      </Drawer.Navigator>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login} options={{headerShown:false}} />
+      </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
 
 // // import 'react-native-gesture-handler'
