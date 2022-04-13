a45721b23719127c9ac7c206ecbb39e355bcb121
Lucas Flaquer <lucas.flaquer@gmail.com>
2022-04-12 22:16:05 -0300

    authenticate View

--- src/views/Authenticate/Login.tsx
+++ src/views/Authenticate/Login.tsx
@@ -1,83 +1,43 @@
 import React from 'react'
 import styled from 'styled-components/native'
 import { DefaultButton } from '../../components/Buttons/DefaultButton'
+import { FacebookButton } from '../../components/Buttons/FacebookButton'
+import { GithubButton } from '../../components/Buttons/GithubButton'
+import { GoogleButton } from '../../components/Buttons/GoogleButton'
 
-const Background = styled.View`
-  background-color: #fff;
-  flex:1;
-  align-items: center;
-  padding: 15px;
-`
-const LoginTitle = styled.Text`
-  margin-top: 71px;
-  text-align: center;
-  font-weight: bold;
-  font-size: 24px;
-  font-family: 'RobotoBlack';
-  margin-bottom: 55px;
-`
-const FieldContainer = styled.View`
-  width: 290px;
-  margin-bottom: 15px;
-`
-const FieldLabel = styled.Text`
-  font-size: 14px;
-  font-family: 'RobotoRegular';
-  margin-bottom: 7px;
-`
-const FieldInput = styled.TextInput`
-  width: 100%;
-  height: 40px;
-  border-radius: 5px;
-  border: 1px solid #3A3A3A;
-`
-const CenteredText = styled.Text`
-  color: #3A3A3A;
-  font-weight: bold;
-
-  font-family: 'RobotoBlack';
-  font-size: 18px;
-  text-align: center;
-  margin-top: 18px;
-  margin-bottom: 15px;
-`
+import * as S from './styles'
 
 export function Login() {
   function handleSubmint() {
     console.log('submit')
   }
   return (
-    <Background>
-      <LoginTitle>Entrar</LoginTitle>
+    <S.Background>
+      <S.LoginTitle>Entrar</S.LoginTitle>
 
-      <FieldContainer>
-        <FieldLabel>E-mail</FieldLabel>
-        <FieldInput />
-      </FieldContainer>
+      <S.FieldContainer>
+        <S.FieldLabel>E-mail</S.FieldLabel>
+        <S.FieldInput />
+      </S.FieldContainer>
 
-      <FieldContainer>
-        <FieldLabel>Senha</FieldLabel>
-        <FieldInput />
-      </FieldContainer>
+      <S.FieldContainer>
+        <S.FieldLabel>Senha</S.FieldLabel>
+        <S.FieldInput />
+      </S.FieldContainer>
       <DefaultButton 
         label='continuar' 
         onPress={handleSubmint} />
-      <CenteredText>
+      <S.CenteredText>
         Ainda não tem conta? Cadastrar
-      </CenteredText>
-      <CenteredText>
+      </S.CenteredText>
+      <S.CenteredText>
         Ou continue com
-      </CenteredText>
-      <DefaultButton 
-        onPress={() => {}} 
-        label='Continue com Facebook' />
-      <DefaultButton 
-        onPress={() => {}} 
-        label='Continue com Facebook' />
-      <DefaultButton 
-        onPress={() => {}} 
-        label='Continue com Facebook' />
+      </S.CenteredText>
+      <FacebookButton />
+      <GoogleButton />
+      <GithubButton />
+      
         
-    </Background>
+    </S.Background>
   )
 }
---
+++ src/views/Authenticate/styles.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components/native"
+
+export const Background = styled.View`
+  background-color: #fff;
+  flex:1;
+  align-items: center;
+  padding: 15px;
+`
+export const LoginTitle = styled.Text`
+  margin-top: 71px;
+  text-align: center;
+  font-weight: bold;
+  font-size: 24px;
+  font-family: 'RobotoBlack';
+  margin-bottom: 55px;
+`
+export const FieldContainer = styled.View`
+  width: 290px;
+  margin-bottom: 15px;
+`
+export const FieldLabel = styled.Text`
+  font-size: 14px;
+  font-family: 'RobotoRegular';
+  margin-bottom: 7px;
+`
+export const FieldInput = styled.TextInput`
+  width: 100%;
+  height: 40px;
+  border-radius: 5px;
+  border: 1px solid #3A3A3A;
+`
+export const CenteredText = styled.Text`
+  color: #3A3A3A;
+  font-weight: bold;
+
+  font-family: 'RobotoBlack';
+  font-size: 18px;
+  text-align: center;
+  margin-top: 18px;
+  margin-bottom: 15px;
+`
