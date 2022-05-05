0995b6569c6bf19d3814d823ef60c71368c9798a
Lucas Flaquer <lucas.flaquer@gmail.com>
2022-05-04 21:37:14 -0300

    ranking screen

---
+++ src/screens/Raking/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BlackTitle } from "../../components/shared/BlackTitle";
+
+export function Raking() {
+  return (
+   <>
+      <BlackTitle>
+        Ranking de econômia de CO2
+100 melhores do ano</BlackTitle>
+    </>
+ )
+}
