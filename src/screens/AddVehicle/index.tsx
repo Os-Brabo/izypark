f675293434aa03d273eb4109cae9c8e42bb1342e
Lucas Flaquer <lucas.flaquer@gmail.com>
2022-05-05 21:17:08 -0300

    lint addVehicle

--- src/screens/AddVehicle/index.tsx
+++ src/screens/AddVehicle/index.tsx
@@ -4,23 +4,26 @@ import { View } from "react-native";
 import { DefaultButton } from "../../components/Buttons/DefaultButton";
 import { Header } from "../../components/Header";
 import { BlackTitle } from "../../components/shared/BlackTitle";
-import { FieldContainer, FieldInput, FieldLabel, FormContainer } from "../../components/shared/Form/styles";
+import {
+  FieldContainer,
+  FieldInput,
+  FieldLabel,
+  FormContainer
+} from "../../components/shared/Form/styles";
 // import * as S from "";
 export function CreateVehicle() {
   function handleSubmit() {}
   const initialValues = {
-    color: '',
-    model: '',
-    plate: '',
-  }
+    color: "",
+    model: "",
+    plate: ""
+  };
   return (
-    <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
       <Header returnTo="Vehicles" />
       <BlackTitle>Cadastrar um Veículo</BlackTitle>
-      <Formik
-        onSubmit={handleSubmit}
-        initialValues={initialValues}>
-        {({handleSubmit}) => (
+      <Formik onSubmit={handleSubmit} initialValues={initialValues}>
+        {({ handleSubmit }) => (
           <FormContainer>
             <FieldContainer>
               <FieldLabel>Placa</FieldLabel>
@@ -34,15 +37,12 @@ export function CreateVehicle() {
               <FieldLabel>Cor</FieldLabel>
               <FieldInput />
             </FieldContainer>
-            <View style={{flex: 1}}></View>
-            <DefaultButton
-              label="Cadastrar"
-              onPress={handleSubmit}
-            />
-            <View style={{height: 50}} />
+            <View style={{ flex: 1 }}></View>
+            <DefaultButton label="Cadastrar" onPress={handleSubmit} />
+            <View style={{ height: 50 }} />
           </FormContainer>
         )}
       </Formik>
     </View>
-  )
+  );
 }
