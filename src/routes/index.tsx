95263e9b35fad479a8bff6fd3ca6db1a9d329b62
Lucas Flaquer <lucas.flaquer@gmail.com>
2022-05-08 00:31:06 -0300

    fixed unused route file and import

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { PublicRoutes } from "./PublicRoutes";
 import { PrivateRoutes } from "./PrivateRoutes";
 
-const Stack = createNativeStackNavigator()
 export function Routes() {
-  const isAuthenticated = true
-  
+  const isAuthenticated = true;
+
   return (
     <NavigationContainer>
-      { isAuthenticated ? <PrivateRoutes /> : <PublicRoutes /> }
+      {isAuthenticated ? <PrivateRoutes /> : <PublicRoutes />}
     </NavigationContainer>
-  )
+  );
 }
