4493263f263c52d028ae60ec1c0c4e634637679f
Lucas Flaquer <lucas.flaquer@gmail.com>
2022-05-08 00:38:05 -0300

    lint

--- src/components/shared/BlackTitle/index.ts
+++ src/components/shared/BlackTitle/index.ts
@@ -1,9 +1,8 @@
-import styled from 'styled-components/native'
+import styled from "styled-components/native";
 
 export const BlackTitle = styled.Text`
   text-align: center;
   font-size: 26px;
-  font-family: 'RobotoBlack';
+  font-family: "RobotoBlack";
   font-weight: bold;
-
-`
+`;
