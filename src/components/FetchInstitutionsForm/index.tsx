64fe4715f6042bcbab311668fba092925155d3eb
Lucas Flaquer <lucas.flaquer@gmail.com>
2022-05-10 20:08:33 -0300

    institutions form

---
+++ src/components/FetchInstitutionsForm/index.tsx
@@ -0,0 +1,25 @@
+import { Formik } from "formik";
+import React from "react";
+import { DefaultButton } from "../Buttons/DefaultButton";
+import { FieldContainer, FieldInput, FieldLabel } from "../shared/Form/styles";
+import * as S from "./styles";
+export function FetchInstitutionsForm() {
+  const initialValues = { searchTerm: "" };
+  function handleSubmit() {
+    console.log("submit");
+  }
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      {({ handleSubmit, handleChange, handleBlur, values }) => (
+        <S.FormContainer>
+          <FieldContainer>
+            <FieldLabel>Instituição</FieldLabel>
+            <FieldInput />
+          </FieldContainer>
+          <DefaultButton label="Buscar" onPress={handleSubmit} />
+        </S.FormContainer>
+      )}
+    </Formik>
+  );
+}
--- src/screens/Institutions/index.tsx
+++ src/screens/Institutions/index.tsx
@@ -1,20 +1,30 @@
 import React from "react";
+import { Text } from "react-native-paper";
 import { DefaultButton } from "../../components/Buttons/DefaultButton";
+import { FetchInstitutionsForm } from "../../components/FetchInstitutionsForm";
 import { Header } from "../../components/Header";
 import { BlackTitle } from "../../components/shared/BlackTitle";
 import { FieldInput, FieldLabel } from "../../components/shared/Form/styles";
+import { Spacer } from "../../components/shared/Spacer";
 import { FieldContainer } from "../publicViews/styles";
 
 export function Institutions() {
+  const institutions = [];
+  const searchMadeOnce = false;
+
   return (
     <>
       <Header returnTo="Home" />
       <BlackTitle>Buscar Instituição</BlackTitle>
-      <FieldContainer>
-        <FieldLabel>Instituição</FieldLabel>
-        <FieldInput />
-      </FieldContainer>
-      <DefaultButton label="Buscar" onPress={() => {}} />
+      <Spacer height={25} />
+      <FetchInstitutionsForm />
+      <Spacer height={25} />
+      {/* list results */}
+      {institutions.length === 0 ? (
+        <Text style={{ textAlign: "center" }}>Nenhum resultado encontrado</Text>
+      ) : (
+        <Text>resultados</Text>
+      )}
     </>
   );
 }
