5db8fdf594c891ce1a70cb1c6b832ea99c1cd648
LucasFlaquer <lucas.flaquer@gmail.com>
2022-06-21 23:30:47 -0300

    navigate

--- src/screens/Institutions/InstitutionItem/index.tsx
+++ src/screens/Institutions/InstitutionItem/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import * as S from "./styles";
 import { AntDesign, Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 export interface Institution {
   id: string;
@@ -14,10 +15,14 @@ interface Props {
 }
 
 export function InstitutionItem({ institution }: Props) {
+  const navigation = useNavigation();
   const iconSize = 30;
   const [isFavorite, setIsFavorite] = React.useState(institution.isFavorite);
   function handleFavoriteItem() {
     setIsFavorite(!isFavorite);
+  }
+  function handleNavigateToDetail() {
+    navigation.navigate("Institutions.Detail" as never, {} as never);
   }
   return (
     <S.Container>
@@ -30,7 +35,7 @@ export function InstitutionItem({ institution }: Props) {
             size={iconSize}
           />
         </TouchableOpacity>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handleNavigateToDetail}>
           <Feather name="info" size={iconSize} color="#3af" />
         </TouchableOpacity>
       </S.Actions>
