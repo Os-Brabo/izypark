dfe0a5ebf3dba32b69d7f67f61669062c3278159
Gustavo Daltoe <gustavodaltoe@hotmail.com>
2022-06-22 22:44:13 -0300

    feat: signout

--- src/routes/DrawerContent.tsx
+++ src/routes/DrawerContent.tsx
@@ -1,13 +1,31 @@
 import React from "react";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItem,
+  DrawerItemList
+} from "@react-navigation/drawer";
 import { Home } from "../screens/Home";
 import { Vehicles } from "../screens/MyVehicles";
 import { Store } from "../screens/Store";
 import { Raking } from "../screens/Raking";
+import { useAuth } from "../hooks/useAuth";
 const Drawer = createDrawerNavigator();
 export function DrawerContent() {
+  const auth = useAuth();
   return (
-    <Drawer.Navigator useLegacyImplementation initialRouteName="Home">
+    <Drawer.Navigator
+      useLegacyImplementation
+      initialRouteName="Home"
+      drawerContent={(props) => {
+        return (
+          <DrawerContentScrollView {...props}>
+            <DrawerItemList {...props} />
+            <DrawerItem label="Sair" onPress={() => auth.signOut()} />
+          </DrawerContentScrollView>
+        );
+      }}
+    >
       <Drawer.Screen
         name="Home"
         component={Home}
